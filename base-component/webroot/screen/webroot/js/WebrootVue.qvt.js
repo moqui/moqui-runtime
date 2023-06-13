b1e3c16957db1df0c22b9f88f1b584472543b9fc
David E Jones <dej@dejc.com>
2023-06-13 18:41:07 -0500

    In WebrootVue.qvt.js add qLayoutMinHeight method to calculate a full height, useful for certain types of screens with particular or bottom instead of top focused layouts

--- base-component/webroot/screen/webroot/js/WebrootVue.qvt.js
+++ base-component/webroot/screen/webroot/js/WebrootVue.qvt.js
@@ -2417,6 +2417,14 @@ moqui.webrootVue = new Vue({
             if (resp.loggedIn) {
                 this.reLoginPostLogin();
             }
+        },
+        qLayoutMinHeight: function(offset) {
+            // "offset" is a Number (pixels) that refers to the total
+            // height of header + footer that occupies on screen,
+            // based on the QLayout "view" prop configuration
+
+            // this is actually what the default style-fn does in Quasar
+            return { minHeight: offset ? `calc(100vh - ${offset}px)` : '100vh' }
         }
     },
     watch: {
